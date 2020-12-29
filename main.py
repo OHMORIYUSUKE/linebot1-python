37da1e51d653677aa3c33231b041901d50441c63
OHMORIYUSUKE <72294945+OHMORIYUSUKE@users.noreply.github.com>
2020-12-29 22:50:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    messages = TextSendMessage(text="えらいね！\nもっとコミットしよーー！")
+    messages = TextSendMessage(text="えらいね！\nもっとコミットしよー！")
     line_bot_api.push_message(USER_ID , messages=messages)
 
 if __name__=="__main__":
